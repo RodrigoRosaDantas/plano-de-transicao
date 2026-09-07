83c1ff0c31b9ee346e3006ab6e85bdf3b03ee8e0
RodrigoRosaDantas <rodrigo.rosa2@outlook.com>
2026-09-07 20:30:24 -0300

    chore: remove JS legado do Dia da Prova v16

--- assets/exam-day-v16.js
+++
@@ -1,3 +0,0 @@
-// Compatibility loader: the site still references this stable entrypoint.
-// The organized Dia da Prova module lives in v18; query v19 forces the audited build.
-import './exam-day-v18.js?v=19';
